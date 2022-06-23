c4fda63b12caa77d964e2bb8417dfb6bf7ff04e3
yossa91 <y0ssa@naver.com>
2022-06-23 16:58:57 +0900

    0623

--- public/js/common.js
+++ public/js/common.js
@@ -1,18 +1,12 @@
 
 
-var famSite = document.querySelector('.familySite>a');
+var famSite = document.querySelector('.fsBtn');
 var famSiteList = document.querySelector('.famSlideMenu');
 var famSLHeight = famSiteList.scrollHeight;
 famSite.addEventListener('click',function(){
     famSiteList.style.height = famSLHeight + 'px';
 });
-document.body.onclick = function(){
-    if(famSiteList.clientHeight >  0){
-        famSiteList.style.height = 0;
-    }else{
-        return false;
-    }
-}
+
 var famSlideDw = document.querySelectorAll('.famSlideMenu>li>a');
 famSlideDw.forEach(function(el,idx){
     famSlideDw[idx].onclick = function(){
